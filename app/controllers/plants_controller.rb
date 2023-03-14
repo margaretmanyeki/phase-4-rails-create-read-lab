3a294b5da118e650ce127576a7caa3900ed802f7
margaret manyeki <maggiemanyeki6@gmail.com>
2023-03-14 20:54:54 +0300

    commit tree 2e4e16228227603ab7ff1abf9890f18db49b7fd8

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,21 @@
+class PlantsController < ApplicationController
+#Get/plants
+    def index
+        plants = Plant.all 
+        render json: plants
+    end
+#GET /plants/:id
+    def show 
+        plant = Plant.find_by(id: params[:id])
+        render json: plant
+    end
+#Post/plants
+    def create 
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end
+    private
+    def plant_params
+        params.permit(:name, :image, :price)   
+    end 
+end
